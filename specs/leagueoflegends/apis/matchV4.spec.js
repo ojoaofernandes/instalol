57f11b90e0f75107864ab970a90916985cb4573e
Joao Fernandes <ojoaofernandes@gmail.com>
2020-05-22 19:15:02 -0300

    feat: add new function to fetch match details to match api v4

--- specs/leagueoflegends/apis/matchV4.spec.js
+++ specs/leagueoflegends/apis/matchV4.spec.js
@@ -4,6 +4,7 @@ import * as http from '../../../src/leagueoflegends/transports/http';
 
 describe('LOL - Match API V4', () => {
   const validAccountId = 'zMZ45BJ1o_acCFqVSnG-vzh1ZDe53uweNAD4JriPyy-TQkr1PfJr51-1';
+  const validMatchId = 56421387;
 
   afterEach(() => {
     sinon.restore();
@@ -18,7 +19,7 @@ describe('LOL - Match API V4', () => {
 
     it('should send the request to the api passing the account ID', () => {
       const spy = sinon.spy(http, 'get');
-      const path = `${api.ENDPOINT_GET_MATCH_LIST_BY_ACCOUNT}/${validAccountId}`;
+      const path = `${api.ENDPOINT_GET_MATCH_LIST_BY_ACCOUNT_ID}/${validAccountId}`;
       api.getMatchListByAccountId(validAccountId);
       expect(spy).to.have.been.calledWith(path);
     });
@@ -30,6 +31,33 @@ describe('LOL - Match API V4', () => {
           json: spy,
         });
         api.getMatchListByAccountId(validAccountId).then(() => {
+          expect(spy).to.have.been.called;
+          done();
+        });
+      });
+  });
+
+  describe('#getMatchByMatchId', () => {
+    it('should validate the match ID', () => {
+      const spy = sinon.spy(helpers, 'validateMatchId');
+      api.getMatchByMatchId(validMatchId);
+      expect(spy).to.have.been.called;
+    });
+
+    it('should send the request to the api passing the match ID', () => {
+      const spy = sinon.spy(http, 'get');
+      const path = `${api.ENDPOINT_GET_MATCH_BY_MATCH_ID}/${validMatchId}`;
+      api.getMatchByMatchId(validMatchId);
+      expect(spy).to.have.been.calledWith(path);
+    });
+
+    it('should resolve the promise and return the response of .json()',
+      (done) => {
+        const spy = sinon.spy();
+        sinon.stub(http, 'get').resolves({
+          json: spy,
+        });
+        api.getMatchByMatchId(validMatchId).then(() => {
           expect(spy).to.have.been.called;
           done();
         });
--- src/leagueoflegends/apis/matchV4.js
+++ src/leagueoflegends/apis/matchV4.js
@@ -1,7 +1,8 @@
-import { validateAccountId } from '../helpers';
+import { validateAccountId, validateMatchId } from '../helpers';
 import { get } from '../transports/http';
 
-export const ENDPOINT_GET_MATCH_LIST_BY_ACCOUNT = 'match/v4/matchlists/by-account';
+export const ENDPOINT_GET_MATCH_LIST_BY_ACCOUNT_ID = 'match/v4/matchlists/by-account';
+export const ENDPOINT_GET_MATCH_BY_MATCH_ID = 'match/v4/matches';
 
 /**
  * Get match list for games played on given account ID.
@@ -13,6 +14,20 @@ export const ENDPOINT_GET_MATCH_LIST_BY_ACCOUNT = 'match/v4/matchlists/by-accoun
  */
 export function getMatchListByAccountId(accountId) {
   validateAccountId(accountId);
-  const path = `${ENDPOINT_GET_MATCH_LIST_BY_ACCOUNT}/${accountId}`;
+  const path = `${ENDPOINT_GET_MATCH_LIST_BY_ACCOUNT_ID}/${accountId}`;
+  return get(path).then((res) => res.json());
+}
+
+/**
+ * Get match by match ID.
+ *
+ * @param {number} matchId The match ID.
+ * @returns {Object} The match object entity.
+ * @throws {TypeError} The match ID must be a number.
+ * @throws {RangeError} The match ID must be a positive number.
+ */
+export function getMatchByMatchId(matchId) {
+  validateMatchId(matchId);
+  const path = `${ENDPOINT_GET_MATCH_BY_MATCH_ID}/${matchId}`;
   return get(path).then((res) => res.json());
 }
